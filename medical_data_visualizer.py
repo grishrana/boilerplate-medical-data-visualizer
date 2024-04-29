a5a9a5669a2981cecc74f7eae99f73ab397bf828
grishrana <grishrana6@gmail.com>
2024-04-29 23:11:42 +0545

    working on it

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,23 +3,36 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+    
+
 # 1
-df = None
+df = pd.read_csv(
+    'medical_examination.csv',
+    index_col='id'
+    )
 
 # 2
-df['overweight'] = None
+BMI=df['weight']/((df['height']/100)**2)
+df['overweight'] = BMI.apply(lambda x: 1 if x>25 else 0)
 
 # 3
-
+df['gluc']=df['gluc'].apply(lambda x: 1 if x>1 else 0)
+df['cholesterol']=df['cholesterol'].apply(lambda x: 1 if x>1 else 0)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = df.melt(id_vars='cardio',value_vars=['cholesterol','gluc','smoke','alco','active','overweight'])
+    df_cat['total']=0
+    variable_list=['cholesterol','gluc','smoke','alco','active','overweight']
+    for variables in variable_list:
+        for cardio in range(2):
+            for variable_val in range(2):
+                df_cat.loc[((df_cat['cardio']==cardio) & (df_cat['variable']==variables) & (df_cat['value']==variable_val)),"total"]=df_cat[(df_cat['cardio']==cardio) & (df_cat['variable']==variables) & (df_cat['value']==variable_val)].shape[0]
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat.drop_duplicates()
     
 
     # 7
@@ -27,7 +40,7 @@ def draw_cat_plot():
 
 
     # 8
-    fig = None
+    fig = sns.catplot(data=df_cat,x='variable',y='total',col='cardio',hue='value',kind='bar')
 
 
     # 9
